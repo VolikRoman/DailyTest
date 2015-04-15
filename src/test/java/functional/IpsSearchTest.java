dfa4baf6b7f3a5340730472615a7f8c59a162282
Roman <volikroman@gmail.com>
2015-04-15 13:07:49 +0300

    Functional test for IPS
    - Search procedure purchase documentation
    - Search non - procedure documentation
    - Functional create/delete profile purchase tracking

--- src/test/java/functional/IpsSearchTest.java
+++ src/test/java/functional/IpsSearchTest.java
@@ -1,159 +1,19 @@
 package functional;
 
-import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-import webdriver.Browser;
+import pages.SearchPage;
 
 public class IpsSearchTest extends AbstractTest {
-    private Browser browser;
-    private static final String BaseURL = "http://ips.vdz.ua/ua/";
-
-    public void login(){
-        browser.get(BaseURL + "/index.php"); // переход на сайт
-        browser.findElement(By.id("login")).sendKeys("VRomanA@i.ua"); // логин
-        browser.findElement(By.id("pwd")).sendKeys("11111");  //пароль
-        browser.findElement(By.name("enter")).click();
-    }
-
-    public void logout(){
-        browser.findElement(By.xpath("//div[@class='enterForm']/span[4]")).click();
-    }
-
-    public void submit(){
-        browser.findElement(By.id("Submit")).click(); //нажатие кнопки "Поиск"
-        Assert.assertEquals(browser.findElement(By.xpath("//a[@class='button btnDefault doTrack']/span")).getText(), "ВІДСТЕЖИТИ"  /*"ВІДСТЕЖУЄТЬСЯ"*/); //проверка наличии кнопки "ВІДСТЕЖИТИ"
-    }
-
-    public void testTitle(){
-        Assert.assertEquals(browser.getTitle(), "Сервіс Пошукових профілів та Відстеження тендерів");
-    }
-
-    public void testSearch(){
-        browser.findElement(By.xpath("//a[1]/img")).click(); //переход на стр. поиска
-        Assert.assertEquals(browser.findElement(By.cssSelector("h1.blockTitle.tab1")).getText(), "ЗАКУПІВЛІ"); //проверка что находимся на стр. поиска
-        submit();
-    }
-
-    //поиск "Оголошення про початок"
-    public void testAnnouncementStartSearch(){
-        browser.findElement(By.id("area_6")).click();
-        submit();
-    }
-
-    //поиск "Оголошення про результати"
-    public void testAnnouncementResultSearch(){
-        browser.findElement(By.id("area_9")).click();
-        submit();
-    }
-
-    //поиск "Всі документи"
-    public void testAllDocumentsSearch(){
-        browser.findElement(By.id("area_0")).click();
-        submit();
-    }
-
-    //поиск "Відкриті торги"
-    public void testOpenPurchaseSearch(){
-        browser.findElement(By.id("purchase_type_1")).click();
-        submit();
-        browser.findElement(By.id("purchase_type_1")).click();
-    }
-
-    //поиск "Двоступеневі торги"
-    public void testTwoStagePurchaseSearch(){
-        browser.findElement(By.id("purchase_type_3")).click();
-        submit();
-        browser.findElement(By.id("purchase_type_3")).click();
-    }
-
-    //поиск "Запит цінових пропозицій"
-    public void testQuotationSearch(){
-        browser.findElement(By.id("purchase_type_4")).click();
-        submit();
-        browser.findElement(By.id("purchase_type_4")).click();
-    }
-
-    //поиск "Попередня кваліфікація учасників"
-    public void testPrequalificationSearch(){
-        browser.findElement(By.id("purchase_type_7")).click();
-        submit();
-        browser.findElement(By.id("purchase_type_7")).click();
-    }
-
-    //поиск "Переговорна процедура закупівлі"
-    public void testNegotiatingPurchase(){
-        browser.findElement(By.id("purchase_type_11")).click();
-        submit();
-        browser.findElement(By.id("purchase_type_11")).click();
-    }
-
-    //поиск "Інша закупівля"
-    public void testOtherPurchase(){
-        browser.findElement(By.id("purchase_type_10")).click();
-        submit();
-        browser.findElement(By.id("purchase_type_10")).click();
-    }
-
-    //поиск "Закупівля в одного учасника"
-    public void testPurchaseOfOneParticipant(){
-        browser.findElement(By.id("purchase_type_6")).click();
-        submit();
-        browser.findElement(By.id("purchase_type_6")).click();
-    }
-
-    //поиск по ключевому словам
-    public void testByKeyWord(){
-        browser.findElement(By.id("subject")).sendKeys("Газ");
-        submit();
-        browser.findElement(By.id("subject")).clear();
-        browser.findElement(By.id("subject")).sendKeys("Послуги");
-        submit();
-        browser.findElement(By.id("subject")).clear();
-    }
-
-    //поиск по класификатору
-    public void testPurchaseClassifier(){
-        browser.findElement(By.id("purchase_classifier")).sendKeys("01");
-        submit();
-        browser.findElement(By.id("subject")).clear();
-    }
-    @Test
-    public void testIpsPurchaseSearchDaily() {
-        login();
-        testTitle();
-        testSearch();
-        testAnnouncementStartSearch();
-        testAnnouncementResultSearch();
-        testAllDocumentsSearch();
-        testOpenPurchaseSearch();
-        testTwoStagePurchaseSearch();
-        testQuotationSearch();
-        testPrequalificationSearch();
-        testNegotiatingPurchase();
-        testOtherPurchase();
-        testPurchaseOfOneParticipant();
-        testByKeyWord();
-        testPurchaseClassifier();
-
-        logout();
-    }
 
     @Test
-    public void testIpsDocumentSearchDaily() {
-        login();
-        browser.findElement(By.xpath("//a[1]/img")).click(); //переход на стр. поиска
-        //Непроцедурні документи
-//        Assert.assertEquals(browser.findElement(By.cssSelector("h1.blockTitle.tab2")).getText(), "НЕПРОЦЕДУРНІ ДОКУМЕНТИ");
-        browser.findElement(By.cssSelector("h1.blockTitle.tab2")).click();
-//        browser.findElement(By.xpath("//div[@class='searchForm']/.//button[@id='Submit']/span")).click();
-        browser.findElement(By.cssSelector("td > #Submit")).click();
-
-        browser.findElement(By.name("p_user")).sendKeys("37927092");
-        browser.findElement(By.className("btnDefault")).click();
-        Assert.assertEquals(browser.findElement(By.xpath("//a[@class='btnDefault']/span")).getText(), "ФАЙЛ");
-        browser.findElement(By.name("p_user")).clear();
-        logout();
+    public void test(){
+        SearchPage searchPage = new SearchPage(browser);
+        searchPage.openHomePage();
+        searchPage.login();
+        searchPage.openSearchPage();
+        searchPage.testIpsPurchaseSearchDaily();
+        searchPage.testIpsDocumentSearchDaily();
+        searchPage.logout();
     }
 
 }
--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -1,6 +1,9 @@
 package pages;
 
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import webdriver.Browser;
 
 /**
@@ -18,4 +21,146 @@ public class SearchPage extends AbstractPage {
     public boolean isOpened() {
         return browser.findElement(RESET_BUTTON).isDisplayed();
     }
+
+    public void submit(){
+        browser.findElement(By.id("Submit")).click(); //нажатие кнопки "Поиск"
+        Assert.assertEquals(browser.findElement(By.xpath("//a[@class='button btnDefault doTrack']/span")).getText(), "ВІДСТЕЖИТИ"  /*"ВІДСТЕЖУЄТЬСЯ"*/); //проверка наличии кнопки "ВІДСТЕЖИТИ"
+    }
+
+    public void testTitle(){
+        Assert.assertEquals(browser.getTitle(), "Пошук тендерів");
+    }
+
+    public void testSearch(){
+        browser.findElement(By.xpath("//a[1]/img")).click(); //переход на стр. поиска
+        Assert.assertEquals(browser.findElement(By.cssSelector("h1.blockTitle.tab1")).getText(), "ЗАКУПІВЛІ"); //проверка что находимся на стр. поиска
+        submit();
+    }
+
+    public void testRadioButton(By by){
+        browser.findElement(by).click();
+        submit();
+    }
+
+    //поиск "Оголошення про початок"
+    public void testAnnouncementStartSearch(){
+        testRadioButton(By.id("area_6"));
+    }
+
+    //поиск "Оголошення про результати"
+    public void testAnnouncementResultSearch(){
+        testRadioButton(By.id("area_9"));
+    }
+
+    //поиск "Всі документи"
+    public void testAllDocumentsSearch(){
+        testRadioButton(By.id("area_0"));
+    }
+
+    public void testCheckBox(By by){
+        browser.findElement(by).click();
+        submit();
+        browser.findElement(by).click();
+    }
+
+    //поиск "Відкриті торги"
+    public void testOpenPurchaseSearch(){
+        testCheckBox(By.id("purchase_type_1"));
+    }
+
+    //поиск "Двоступеневі торги"
+    public void testTwoStagePurchaseSearch(){
+        testCheckBox(By.id("purchase_type_3"));
+    }
+
+    //поиск "Запит цінових пропозицій"
+    public void testQuotationSearch(){
+        testCheckBox(By.id("purchase_type_4"));
+    }
+
+    //поиск "Попередня кваліфікація учасників"
+    public void testPrequalificationSearch(){
+        testCheckBox(By.id("purchase_type_7"));
+    }
+
+    //поиск "Переговорна процедура закупівлі"
+    public void testNegotiatingPurchase(){
+        testCheckBox(By.id("purchase_type_11"));
+    }
+
+    //поиск "Інша закупівля"
+    public void testOtherPurchase(){
+        testCheckBox(By.id("purchase_type_10"));
+    }
+
+    //поиск "Закупівля в одного учасника"
+    public void testPurchaseOfOneParticipant(){
+        testCheckBox(By.id("purchase_type_6"));
+    }
+
+    public void testText(By by, String str){
+        browser.findElement(by).sendKeys(str);
+        submit();
+        browser.findElement(by).clear();
+    }
+
+    //поиск по ключевому словам
+    public void testByKeyWord(){
+        testText(By.id("subject"), "Газ");
+        testText(By.id("subject"), "Послуги");
+    }
+
+    //поиск по класификатору
+    public void testPurchaseClassifier(){
+        testText(By.id("purchase_classifier"), "01");
+    }
+
+    public void testIpsPurchaseSearchDaily() {
+        testTitle();
+        testSearch();
+        testAnnouncementStartSearch();
+        testAnnouncementResultSearch();
+        testAllDocumentsSearch();
+        testOpenPurchaseSearch();
+        testTwoStagePurchaseSearch();
+        testQuotationSearch();
+        testPrequalificationSearch();
+        testNegotiatingPurchase();
+        testOtherPurchase();
+        testPurchaseOfOneParticipant();
+        testByKeyWord();
+        testPurchaseClassifier();
+    }
+
+    public void testIpsDocumentSearchDaily() {
+        browser.findElement(By.xpath("//a[1]/img")).click(); //переход на стр. поиска
+        //Непроцедурні документи
+        Assert.assertEquals(browser.findElement(By.cssSelector("h1.blockTitle.tab2")).getText(), "НЕПРОЦЕДУРНІ ДОКУМЕНТИ");
+        browser.findElement(By.cssSelector("h1.blockTitle.tab2")).click();
+        browser.findElement(By.id("Submit_nonprocedural")).click();
+        Assert.assertEquals(browser.findElement(By.xpath("//a[@class='btnDefault']/span")).getText(), "ФАЙЛ");
+
+        new Select(browser.findElement(By.className("p_var_region"))).selectByValue("1");
+        browser.findElement(By.id("Submit_nonprocedural")).click();
+        Assert.assertEquals(browser.findElement(By.xpath("//a[@class='btnDefault']/span")).getText(), "ФАЙЛ");
+        new Select(browser.findElement(By.className("p_var_region"))).selectByValue("-1");
+        browser.findElement(By.id("Submit_nonprocedural")).click();
+
+        new Select(browser.findElement(By.className("type_document"))).selectByValue("1");
+        browser.findElement(By.id("Submit_nonprocedural")).click();
+        Assert.assertEquals(browser.findElement(By.xpath("//a[@class='btnDefault']/span")).getText(), "ФАЙЛ");
+        new Select(browser.findElement(By.className("type_document"))).selectByValue("2");
+        browser.findElement(By.id("Submit_nonprocedural")).click();
+        Assert.assertEquals(browser.findElement(By.xpath("//a[@class='btnDefault']/span")).getText(), "ФАЙЛ");
+        new Select(browser.findElement(By.className("type_document"))).selectByValue("3");
+        browser.findElement(By.id("Submit_nonprocedural")).click();
+        Assert.assertEquals(browser.findElement(By.xpath("//a[@class='btnDefault']/span")).getText(), "ФАЙЛ");
+
+
+//        browser.findElement(By.name("p_user")).sendKeys("37927092");
+//        browser.findElement(By.id("Submit_nonprocedural")).click();
+//        Assert.assertEquals(browser.findElement(By.xpath("//a[@class='btnDefault']/span")).getText(), "ФАЙЛ");
+//        browser.findElement(By.name("p_user")).clear();
+    }
+
 }
